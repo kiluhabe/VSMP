c0ff0ee516ed0be3590d8e0403fa5e6992f51b16
kiluhabe <kiluhabe@users.noreply.github.com>
2019-08-30 22:36:16 +0900

    try thread

--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,39 @@ mod epd7in5;
 use rppal::spi::{Spi, Mode, Bus, SlaveSelect};
 use rppal::gpio::Gpio;
 use image::{ImageBuffer, Luma};
+use std::thread;
 
 use epd_interface::EPDInterface;
 use epd7in5::EPD;
 use errors::VSMPError;
 
-fn main() -> Result<(), VSMPError>{
-    let gpio = Gpio::new().unwrap();
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 2000000, Mode::Mode0).unwrap();
+fn main() {
+    let _ = thread::spawn(|| -> Result<(), VSMPError> {
+        let gpio = Gpio::new().unwrap();
+        let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 2000000, Mode::Mode0).unwrap();
 
-    let epd_interface = EPDInterface {
-        gpio: gpio, spi: spi
-    };
-    let mut epd = EPD {
-        height: 384,
-        width: 640,
-        interface: epd_interface
-    };
+        let epd_interface = EPDInterface {
+            gpio: gpio, spi: spi
+        };
+        let mut epd = EPD {
+            height: 384,
+            width: 640,
+            interface: epd_interface
+        };
 
-    let img = ImageBuffer::from_fn(640, 384, |x, y| {
-        if x % 2 == 0 {
-            Luma([0u8])
-        } else {
-            Luma([255u8])
-        }
-    });
-    let buffer = epd.get_frame_buffer(img);
+        let img = ImageBuffer::from_fn(640, 384, |x, y| {
+            if x % 2 == 0 {
+                Luma([0u8])
+            } else {
+                Luma([255u8])
+            }
+        });
+        let buffer = epd.get_frame_buffer(img);
 
-    epd.init()?;
-    epd.display_frame(&buffer)?;
+        epd.init()?;
+        epd.display_frame(&buffer)?;
+        epd.interface.sleep_ms(10000);
 
-    epd.interface.sleep_ms(10000);
-
-    Ok(())
+        Ok(())
+    }).join();
 }
