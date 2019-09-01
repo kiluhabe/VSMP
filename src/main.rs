de9da3a7b4f3a25d58d174d7b87a738c23360343
kiluhabe <kiluhabe@users.noreply.github.com>
2019-09-01 23:43:31 +0900

    remove invert

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,7 @@ fn init_epd(height: u32, width: u32) -> EPD {
 
 fn get_image_buffer(path_str: &str, width: u32, height: u32) -> Result<Vec<u8>, VSMPError> {
     let image_path = Path::new(path_str);
-    let mut img = image::open(&image_path)?;
-    img.invert();
+    let img = image::open(&image_path)?;
     let resized_image = img.resize_exact(width / 2, height, FilterType::Lanczos3);
     let buffer = resized_image.grayscale().to_luma().to_vec();
     Ok(buffer)
