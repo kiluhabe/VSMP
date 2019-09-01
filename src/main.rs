461f84970cf7f082bfda268add0e856c91504041
kiluhabe <kiluhabe@users.noreply.github.com>
2019-09-02 04:27:25 +0900

    use dither

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,14 @@ fn get_image_buffer(path_str: &str, width: u32, height: u32) -> Result<Vec<u8>, 
     let img = image::open(&image_path)?;
     let resized_image = img.resize_exact(
         width / 2, height, FilterType::Lanczos3);
-    let buffer = resized_image
+    let mut gray_image = resized_image
         .grayscale()
-        .adjust_contrast(50.0)
-        .to_luma()
+        .to_luma();
+    image::imageops::dither(
+        &mut gray_image,
+        &image::imageops::colorops::BiLevel
+    );
+    let buffer = gray_image
         .to_vec()
         .into_iter().map(|e| {
             if e == 255 { 0x03 } else { e }
